52ac29d2434a877fcf27367f179b2e13ede4dcc5
Sinh Ngo <kaka.ngo@gmail.com>
2019-03-24 17:08:02 +0700

    config for heroku deploy

--- dist/index.js
+++ dist/index.js
@@ -7,7 +7,7 @@ const dotenv_1 = __importDefault(require("dotenv"));
 const app_1 = __importDefault(require("./app"));
 const errorLogger_1 = require("./utils/errorLogger");
 dotenv_1.default.config();
-const PORT = process.env.SERVER_PORT;
+const PORT = process.env.PORT;
 // Error handling at Nodejs level
 process.on("uncaughtException", ex => {
     errorLogger_1.errorLogger.error(ex.message, ex);
